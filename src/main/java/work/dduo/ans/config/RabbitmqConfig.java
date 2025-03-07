a3e68c32b8993958c71faa7518e05aa7eb3fca1c
Cute Puppy Dduo <164348579+Dddddduo@users.noreply.github.com>
2025-03-07 14:07:18 +0800

    封装rabbitMQ 并且跑通测试

--- src/main/java/work/dduo/ans/config/RabbitmqConfig.java
+++ src/main/java/work/dduo/ans/config/RabbitmqConfig.java
@@ -1,5 +1,7 @@
 package work.dduo.ans.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
@@ -7,15 +9,25 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitmqConfig {
+
     // 定义队列
     @Bean
-    public Queue demoQueue() {
-        return new Queue("demo.queue", true); // 持久化队列
+    public Queue balloonWordsQueue() {
+        return new Queue("balloonWords.queue", true); // 持久化队列
     }
 
     // 定义直连交换机
     @Bean
-    public DirectExchange demoExchange() {
-        return new DirectExchange("demo.exchange");
+    public DirectExchange balloonWordsExchange() {
+        return new DirectExchange("balloonWords.exchange");
     }
+
+    // 未绑定队列到交换机
+    @Bean
+    public Binding demoBinding() {
+        return BindingBuilder.bind(balloonWordsQueue())
+                .to(balloonWordsExchange())
+                .with("balloonWords.routingKey"); // 需与发送时routingKey一致
+    }
+
 }
